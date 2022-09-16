1794d2c6e7cc01f059e4a1c5e520b9134d87dc2c
Serhii Voitovych <vsv0218@gmail.com>
2022-09-16 11:44:07 +0300

    Add count limit to metaship staking

--- contracts/MetashipStaking.sol
+++ contracts/MetashipStaking.sol
@@ -11,29 +11,42 @@ contract MetashipStaking is Ownable {
 
     IERC721 metashipsAddress;
     mapping(address => mapping(uint => uint)) public stakes;
+    mapping(address => uint) public addressStakingCount;
+    uint32 public stakingCountLimit = 3;
     uint32 oneDayInSeconds = 60*60*24;
+
+    function setStakingCountLimit(uint32 newStakingCountLimit) external onlyOwner {
+        stakingCountLimit = newStakingCountLimit;
+    }
 
     function setNftContractAddress(address _nftContract) external onlyOwner {
         metashipsAddress = IERC721(_nftContract);
     }
 
     function stake1Day(uint tokenId) external payable {
+        require(addressStakingCount[msg.sender] < stakingCountLimit, "Increased staking count limit");
+        
         uint stakeTill = block.timestamp + oneDayInSeconds;
         metashipsAddress.transferFrom(msg.sender, address(this), tokenId);
         stakes[msg.sender][tokenId] = stakeTill;
+        addressStakingCount[msg.sender] += 1;
         emit MetashipStaked(msg.sender, tokenId, block.timestamp, stakeTill);
     }
 
     function stake100Day(uint tokenId) external payable {
+        require(addressStakingCount[msg.sender] < stakingCountLimit, "Increased staking count limit");
+
         uint stakeTill = block.timestamp + oneDayInSeconds * 100;
         metashipsAddress.transferFrom(msg.sender, address(this), tokenId);
         stakes[msg.sender][tokenId] = stakeTill;
+        addressStakingCount[msg.sender] += 1;
         emit MetashipStaked(msg.sender, tokenId, block.timestamp, stakeTill);
     }
 
     function unstake(uint tokenId) external payable {
         uint stakeTill = stakes[msg.sender][tokenId];
         metashipsAddress.transferFrom(address(this), msg.sender, tokenId);
+        addressStakingCount[msg.sender] -= 1;
 
         emit MetashipUnstaked(msg.sender, tokenId, block.timestamp, stakeTill);
     }
