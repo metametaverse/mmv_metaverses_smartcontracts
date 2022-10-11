7d7316540614233dd9f1753636beb04947364295
Serhii Voitovych <vsv0218@gmail.com>
2022-10-11 16:14:31 +0300

    Adjust keyHash

--- contracts/RandomMetashipMint.sol
+++ contracts/RandomMetashipMint.sol
@@ -15,8 +15,9 @@ contract MintRandomNft is Ownable, VRFConsumerBaseV2 {
         uint256[] tokenIds
     );
 
+    //TODO CHANGE FOR MAINNET
     bytes32 keyHash =
-        0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+        0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
     uint64 subscriptionId;
     VRFCoordinatorV2Interface COORDINATOR;
 
